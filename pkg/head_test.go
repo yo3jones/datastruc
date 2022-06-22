0e4926ad92166c420a0256f33692835cb156555b
Chris Jones <chris@Chriss-MacBook-Air.local>
2022-06-22 10:09:20 -0700

    add heap data structure

---
+++ pkg/head_test.go
@@ -0,0 +1,28 @@
+package datastruc
+
+import "testing"
+
+func TestHeap(t *testing.T) {
+	testLesser := func(i, j int) bool {
+		return i < j
+	}
+
+	heap := NewHeap(testLesser, 10, 1, 8)
+
+	heap.Push(5)
+	heap.Push(7)
+	heap.Push(3)
+	heap.Push(2)
+	heap.Push(2)
+
+	expectResults := []int{1, 2, 2, 3, 5, 7, 8, 10}
+	var got int
+	for _, expect := range expectResults {
+		if got = heap.Peak(); got != expect {
+			t.Errorf("expected %d but got %d", expect, got)
+		}
+		if got = heap.Pop(); got != expect {
+			t.Errorf("expected %d but got %d", expect, got)
+		}
+	}
+}
---
+++ pkg/heap.go
@@ -0,0 +1,127 @@
+package datastruc
+
+import "sync"
+
+type Heap[T any] interface {
+	Peak() T
+	Pop() T
+	Push(v T)
+}
+
+type heap[T any] struct {
+	lesser func(i, j T) bool
+	data   []T
+	lock   *sync.Mutex
+}
+
+func NewHeap[T any](lesser func(i, j T) bool, initialValues ...T) Heap[T] {
+	heap := &heap[T]{
+		lesser: lesser,
+		data:   make([]T, 0, len(initialValues)),
+		lock:   &sync.Mutex{},
+	}
+
+	heap.data = append(heap.data, initialValues...)
+	dataLen := len(heap.data)
+	for i := dataLen - 1; i >= dataLen/2; i-- {
+		heap.pushUp(i)
+	}
+
+	return heap
+}
+
+func (heap *heap[T]) Peak() T {
+	heap.lock.Lock()
+	defer heap.lock.Unlock()
+
+	return heap.data[0]
+}
+
+func (heap *heap[T]) Pop() T {
+	heap.lock.Lock()
+	defer heap.lock.Unlock()
+
+	value := heap.data[0]
+	lastIndex := len(heap.data) - 1
+	heap.data[0] = heap.data[lastIndex]
+	heap.data = heap.data[:lastIndex]
+
+	heap.pushDown(0)
+
+	return value
+}
+
+func (heap *heap[T]) Push(v T) {
+	heap.lock.Lock()
+	defer heap.lock.Unlock()
+
+	heap.data = append(heap.data, v)
+	heap.pushUp(len(heap.data) - 1)
+}
+
+func (heap *heap[T]) pushUp(index int) {
+	for {
+		if index <= 0 {
+			return
+		}
+
+		parentIndex := index / 2
+
+		value := heap.data[index]
+		parentValue := heap.data[parentIndex]
+
+		if !heap.lesser(heap.data[index], heap.data[parentIndex]) {
+			return
+		}
+
+		heap.data[parentIndex] = value
+		heap.data[index] = parentValue
+
+		index = parentIndex
+	}
+}
+
+func (heap *heap[T]) pushDown(index int) {
+	for {
+		dataLen := len(heap.data)
+		if index >= dataLen {
+			return
+		}
+
+		leftIndex := (index * 2) + 1
+		rightIndex := (index * 2) + 2
+
+		var lessIndex int
+		var lessValue T
+
+		if leftIndex >= dataLen {
+			// is a leaf
+			return
+		} else if rightIndex >= dataLen {
+			// only one child
+			lessIndex = leftIndex
+			lessValue = heap.data[leftIndex]
+		} else {
+			// two children
+			leftValue := heap.data[leftIndex]
+			rightValue := heap.data[rightIndex]
+			if heap.lesser(leftValue, rightValue) {
+				lessIndex = leftIndex
+				lessValue = leftValue
+			} else {
+				lessIndex = rightIndex
+				lessValue = rightValue
+			}
+		}
+
+		value := heap.data[index]
+
+		if !heap.lesser(lessValue, value) {
+			return
+		}
+
+		heap.data[index] = lessValue
+		heap.data[lessIndex] = value
+		index = lessIndex
+	}
+}
